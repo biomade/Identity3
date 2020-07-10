5de748a991ddb867846da4109804a7846f23aec9
laurie kern <laurie.kern.ctr@us.af.mil>
2020-07-10 10:48:36 -0500

    add role based authorization

--- Identity3/Pages/Special/Index.cshtml.cs
+++ Identity3/Pages/Special/Index.cshtml.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Identity3.Pages.Special
 {
+    [Authorize(Roles = "Administrator")]
     public class IndexModel : PageModel
     {
+        
+        
+        public IndexModel()
+        {
+            //var inRole = User.IsInRole("Administrator");
+            //This is null because the Principle user has not been created
+        }
         public void OnGet()
         {
-
+            
         }
     }
 }
--- Identity3/Startup.cs
+++ Identity3/Startup.cs
@@ -31,6 +31,8 @@ namespace Identity3
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddRoleManager<RoleManager<IdentityRole>>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages()
                 .AddRazorPagesOptions(options=>
